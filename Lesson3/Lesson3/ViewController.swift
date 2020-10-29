7f41e2c485765715a9615b66029939d3447ecb93
Egor Petrov <egor@barbatosso.com>
2020-10-29 20:13:45 +0300

    First commit

---
+++ Lesson3/Lesson3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson3
+//
+//  Created by Egor Petrov on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
